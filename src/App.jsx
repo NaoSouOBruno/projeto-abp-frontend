d71aacf54e79d0988695966cfa8e17cd09aea616
joao1824 <joao.1824@alunosatc.edu.br>
2025-05-28 09:29:43 -0300

    React + componente do Login

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import {login} from "./components/login.jsx";
+
+
+function App() {
+  return (
+      login()      
+  )
+}
+
+export default App;
---
+++ src/components/login.jsx
@@ -0,0 +1,23 @@
+import './login.css'
+
+export function login(){
+  
+
+  return (
+     <div className = 'login_div'>
+        <div className = "login_div2">
+          <img src="/public/logo.png" alt="dog" />
+        </div>
+        <div className = 'login_user'>
+          <form action="">
+              <img src="/src/assets/user.png" alt="user" />
+              <input className = 'login_usuario' name = "Usuario" type="text" placeholder="Usuário" required/>
+              <img src="/src/assets/senha.png" alt="senha" />
+              <input className = 'login_senha' name = "Senha" type="password" placeholder="Senha" required/>
+              <button  type="submit" >Acessar</button>
+          </form>
+        </div>
+      </div>
+    );
+}
+
