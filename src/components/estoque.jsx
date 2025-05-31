08cffe3df92096f02b9d43835dadd9640e7b1936
joao1824 <joao.1824@alunosatc.edu.br>
2025-05-31 20:48:52 -0300

    melhorando a responsividade

--- src/components/estoque.jsx
+++ src/components/estoque.jsx
@@ -4,7 +4,7 @@ import { Item } from "./card/item";
 const DB_ITEM = [
          {
             imagem:'/src/assets/estoque/ração.jpg',
-            nome:'Teste',
+            nome:'Testse',
              codigo:"34534",
              unidade:"pacote",
              quantidade:12,
@@ -70,24 +70,26 @@ export function estoque(){
     return(
           <div id = "estoque">
             <h1>Estoque</h1>
-            <table>
-                <tr id = "estoque_info">
-                    <th>Imagem</th>
-                    <th>Nome</th>
-                    <th>Código</th>
-                    <th>Unidade</th>
-                    <th>Quantidade</th>
-                </tr>
-                {DB_ITEM.map((itens) => (
-                    <tr id = "estoque_dados" >
-                        <th><img src={itens.imagem}/></th>
-                        <th>{itens.nome}</th>
-                        <th>{itens.codigo}</th>
-                        <th>{itens.unidade}</th>
-                        <th>{itens.quantidade}</th>
+            <div id = "div_tabela">
+                <table>
+                    <tr>
+                        <th>Imagem</th>
+                        <th>Nome</th>
+                        <th>Código</th>
+                        <th>Unidade</th>
+                        <th>Quantidade</th>
                     </tr>
-                 ) ) } 
-            </table>
+                    {DB_ITEM.map((itens) => (
+                        <tr>
+                            <td><img src={itens.imagem}/></td>
+                            <td>{itens.nome}</td>
+                            <td>{itens.codigo}</td>
+                            <td>{itens.unidade}</td>
+                            <td>{itens.quantidade}</td>
+                        </tr>
+                    ) ) } 
+                </table>
+            </div>
             
             
             
