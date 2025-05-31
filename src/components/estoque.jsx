a34f4e9420e2c20568ff63f7e1b74776773b82a9
joao1824 <joao.1824@alunosatc.edu.br>
2025-05-31 18:03:59 -0300

    em formato de tabela

--- src/components/estoque.jsx
+++ src/components/estoque.jsx
@@ -2,19 +2,163 @@ import './estoque.css'
 import { Item } from "./card/item";
 
 const DB_ITEM = [
-        {
+         {
             imagem:'/src/assets/estoque/ração.jpg',
             nome:'Teste',
+             codigo:"34534",
+             unidade:"pacote",
+             quantidade:12,
         },
-       
+        {
+            //  imagem:'/src/assets/estoque/ração.jpg',
+            imagem:"asa",
+            nome:'Teste',
+             codigo:"34534",
+             unidade:"pacote",
+             quantidade:12,
+        },
+        {
+            //  imagem:'/src/assets/estoque/ração.jpg',
+            imagem:"asa",
+            nome:'Teste',
+             codigo:"34534",
+             unidade:"pacote",
+             quantidade:12,
+        },
+        {
+            //  imagem:'/src/assets/estoque/ração.jpg',
+            imagem:"asa",
+            nome:'Teste',
+             codigo:"34534",
+             unidade:"pacote",
+             quantidade:12,
+        },
+        {
+            //  imagem:'/src/assets/estoque/ração.jpg',
+            imagem:"asa",
+            nome:'Teste',
+             codigo:"34534",
+             unidade:"pacote",
+             quantidade:12,
+        },
+        {
+            //  imagem:'/src/assets/estoque/ração.jpg',
+            imagem:"asa",
+            nome:'Teste',
+             codigo:"34534",
+             unidade:"pacote",
+             quantidade:12,
+        },
+        {
+            //  imagem:'/src/assets/estoque/ração.jpg',
+            imagem:"asa",
+            nome:'Teste',
+             codigo:"34534",
+             unidade:"pacote",
+             quantidade:12,
+        },
+        {
+            //  imagem:'/src/assets/estoque/ração.jpg',
+            imagem:"asa",
+            nome:'Teste',
+             codigo:"34534",
+             unidade:"pacote",
+             quantidade:12,
+        },
+        {
+            //  imagem:'/src/assets/estoque/ração.jpg',
+            imagem:"asa",
+            nome:'Teste',
+             codigo:"34534",
+             unidade:"pacote",
+             quantidade:12,
+        },
+        {
+            //  imagem:'/src/assets/estoque/ração.jpg',
+            imagem:"asa",
+            nome:'Teste',
+             codigo:"34534",
+             unidade:"pacote",
+             quantidade:12,
+        },
+        {
+            //  imagem:'/src/assets/estoque/ração.jpg',
+            imagem:"asa",
+            nome:'Teste',
+             codigo:"34534",
+             unidade:"pacote",
+             quantidade:12,
+        },
+        {
+            //  imagem:'/src/assets/estoque/ração.jpg',
+            imagem:"asa",
+            nome:'Teste',
+             codigo:"34534",
+             unidade:"pacote",
+             quantidade:12,
+        },
+        {
+            //  imagem:'/src/assets/estoque/ração.jpg',
+            imagem:"asa",
+            nome:'Teste',
+             codigo:"34534",
+             unidade:"pacote",
+             quantidade:12,
+        },
+        {
+            //  imagem:'/src/assets/estoque/ração.jpg',
+            imagem:"asa",
+            nome:'Teste',
+             codigo:"34534",
+             unidade:"pacote",
+             quantidade:12,
+        },
+        {
+            //  imagem:'/src/assets/estoque/ração.jpg',
+            imagem:"asa",
+            nome:'Teste',
+             codigo:"34534",
+             unidade:"pacote",
+             quantidade:12,
+        },
+        {
+            //  imagem:'/src/assets/estoque/ração.jpg',
+            imagem:"asa",
+            nome:'Teste',
+             codigo:"34534",
+             unidade:"pacote",
+             quantidade:12,
+        },
     ];
 export function estoque(){
     return(
           <div id = "estoque">
-            {DB_ITEM.map((itens) => (
-                <Item imagem={itens.imagem} nome = {itens.nome}>    
-                </Item>
-            ) ) }
+            <h1>Estoque</h1>
+            <table>
+                <tr id = "estoque_info">
+                    <th>Imagem</th>
+                    <th>Nome</th>
+                    <th>Código</th>
+                    <th>Unidade</th>
+                    <th>Quantidade</th>
+                </tr>
+                {DB_ITEM.map((itens) => (
+                    <tr id = "estoque_dados" >
+                        <th><img src={itens.imagem}/></th>
+                        <th>{itens.nome}</th>
+                        <th>{itens.codigo}</th>
+                        <th>{itens.unidade}</th>
+                        <th>{itens.quantidade}</th>
+                    </tr>
+                 ) ) } 
+            </table>
+            
+            
+            
+            
+            
+            
+            
           </div>
     );
 }
