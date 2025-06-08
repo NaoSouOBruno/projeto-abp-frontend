56a0745ca804a15f5fdc027b51251c127c721ca3
joao1824 <joao.1824@alunosatc.edu.br>
2025-06-08 16:20:16 -0300

    Botão de modificar funcional (:

--- src/components/estoque/edit_estoque.jsx
+++ src/components/estoque/edit_estoque.jsx
@@ -1,38 +1,85 @@
 import "./add_estoque.css"
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export function Edit({openEdit,setOpenEdit,codigoSelect}){
-    const DB_ITEM = JSON.parse(localStorage.getItem("DB_ITEM"))
-    const [Img,setImg] = useState("") //usado para conseguirmos alterar a img
+    
+    
 
-    function AddImagem(event){ //pega a img do input e trasforma em url
-        if (Dado != null && DB_ITEM != null){  
-            const x = event.target.files[0]
-            const xurl = URL.createObjectURL(x);
-            setImg(xurl)
-        }
-        
-    }
+
 
     if (!openEdit) return null;
+        
+        const DB_ITEM = JSON.parse(localStorage.getItem("DB_ITEM"));
+        const [Img,setImg] = useState("") //usado para conseguirmos alterar a img
+        const [item_edit,setItem_edit] = useState([""]);
 
-    const Dado = DB_ITEM.find(DB_ITEM => DB_ITEM.codigo === codigoSelect); // declaro isso aqui porque antes ele da erro que não consegue usar find em null
+    
+        const Dado = DB_ITEM.find(item => item.codigo === codigoSelect) || []; 
+
+        useEffect(() => {
+            setItem_edit(Dado)
+        },[])    
+        
+        function AddImagem(e){ //pega a img do input e trasforma em url
+            if (Dado != null && DB_ITEM != null){
+                const x = e.target.files[0]
+                const xurl = URL.createObjectURL(x);
+                setImg(xurl)
+            }
+        }
+
+        
+        
+        
+
+        const item_construcao = (e) => {   //constroi o item quando o input recebe alteração
+            if (e.target.type === "file"){ // possibilita armazenar um imagem no local storage convertendo ela para ser possivel de leitura
+                const name = e.target.name
+                const imagem = e.target.files[0]; // acho a imagem
+                const imagem_leitor = new FileReader(); // crio o leitor
+                imagem_leitor.readAsDataURL(imagem); // aqui ta lendo, e vai gera um rrsultado que vai ser usando no imagem_leitor.result;
+                imagem_leitor.onloadend = () => {  //espero carregar p imagem_leitor.readAsDataURL(imagem); para fazer isso
+                    const valor = imagem_leitor.result; // com o resultado do imagem_leitor.readAsDataURL(imagem) ele pode já ser amarzenado no local storagem, porque se não for assim  ele não funfa
+                    setItem_edit((antes) => ({
+                    ...antes,[name]: valor
+                    }))
+                }
+
+            }else{
+            const {name,value} = e.target; // caso não for a imagem ele armazena normalmente
+            setItem_edit((antes) => ({
+            ...antes,[name]: value}));}}
+
+
+
+    const salvar_item = () => { // modifica o item e manda para o local storage
+        const item_atualizado = DB_ITEM.map((item) => item.codigo === Dado.codigo ? {...item, imagem:item_edit.imagem, descricao:item_edit.descricao, unidade: item_edit.unidade} : item); //modifica
+        console.log(item_atualizado) // teste para ver se tá chegando
+        localStorage.setItem("DB_ITEM",JSON.stringify(item_atualizado)); //manda
+        setOpenEdit(false); //fecha
+        window.location.reload(false); //autualiza janela
+        }
+
+
 
     return(
 
-        <div id="fundo">    
+        <div id="fundo">
                 <div id="menu">
                     <h1>Editor de item</h1>
                     <form action="">
                     <h2>Imagem:</h2>
-                    <input onChange={AddImagem} type="file" name="imagem"  />
+                    <input onChange={(e) => {
+                        AddImagem(e);
+                        item_construcao(e);
+                    }} type="file" name="imagem"  />
                     <img src={Img === "" ? Dado.imagem: Img} alt="" />
                     <h2>Descrição:</h2>
-                    <input defaultValue={Dado.descricao} name="descricao" type="text" />
+                    <input onChange={item_construcao} defaultValue={Dado.descricao} name="descricao" type="text" />
                     <h2>Código:</h2>
                     <input disabled value={Dado.codigo}  name="codigo" type="text" />
                     <h2>Unidade de controle:</h2>
-                    <select defaultValue={Dado.unidade} name="unidade" type="text">
+                    <select onChange={item_construcao} defaultValue={Dado.unidade} name="unidade" type="text">
                         <option value="Unidade">Unidade</option>
                         <option value="Pacote">Pacote</option>
                         <option value="Caixa">Caixa</option>
@@ -41,12 +88,12 @@ export function Edit({openEdit,setOpenEdit,codigoSelect}){
                         <option value="Rolo">Rolo</option>
                         <option value="Kit">Kit</option>
                     </select>
-                    
+
                     <div id="botoes">
-                        <button>Editar</button> 
+                        <button onClick={salvar_item}>Editar</button>
                         <button onClick={setOpenEdit}>Cancelar</button>
                     </div>
-                    </form> 
+                    </form>
                 </div>
             </div>
 
@@ -55,9 +102,4 @@ export function Edit({openEdit,setOpenEdit,codigoSelect}){
 
 
     )
-
-
- 
-
-
 }
